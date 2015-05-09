55ea6a543ec3d727169282661398f09da60c0b6d
anoopkum <anoopkum@gmail.com>
2015-05-09 11:05:35 +0000

    script to run code on cluster

---
+++ submit_job.pl
@@ -0,0 +1,35 @@
+# Program: submit_job.pl
+# Developed by: Anoop Kumar
+# Date: 05/04/2015
+# Description: The program runs fragile sequence computation on Slurm Cluster
+#
+#!/bin/bash
+
+$| = 1;
+
+$seq_folder =  "/cluster/home/akumar03/fragile/data/hg_18_chr16_1_88827253_r1/";
+$seq_file = "hg_18_chr16_1_88827253.fasta";
+$out_folder = "/cluster/home/akumar03/fragile/data/hg_18_chr16_1_88827253_r1/";
+$START = 0;
+$STOP = 10000000;
+$STEP = 1000000;
+
+for($i = $START;$i<$STOP;$i += $STEP) {
+	$status_file = $out_folder.$seq_file."status_$i.st";
+	$out_file = $out_folder.$seq_file."_$i.txt";
+	$end = $i+$STEP;
+	my $s="#!/bin/bash\n";
+	$s.="#SBATCH --partition=batch\n";
+	$s.="#SBATCH --nodes=1\n";
+	$s.="#SBATCH --ntasks-per-node=1\n";
+	$s.="#SBATCH --mem=4000\n";
+	$s.="perl /cluster/home/akumar03/fragile/code/fragile/find_cruciform.pl  $seq_folder$seq_file $status_file $i $end > $out_file\n";
+	$batch_file  = $out_folder.$seq_file."_$i.sh";	
+	open(BATCH, '>'.$batch_file);
+	print BATCH $s;
+	close BATCH;
+	$cmd = "sbatch $batch_file";	
+	print "Running $cmd\n";
+	`$cmd`;
+}
+
